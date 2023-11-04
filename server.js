dccc719663bc506c18d81dd777e856b81cf37314
samthatcode <samthatcode@gmail.com>
2023-11-04 04:17:18 +0100

    info_sec-bcrypt commit

--- server.js
+++ server.js
@@ -1,53 +1,25 @@
 'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const app         = express();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const app = express();
 fccTesting(app);
+
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-
-//START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
-
-//END_ASYNC
+const bcrypt = require('bcrypt');
 
 //START_SYNC
+// Hash a password synchronously
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log('Hashed Password (Sync):', hash);
 
-
-
+// Compare a plaintext password with the sync hash
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log('Password Match (Sync):', result); // true
 //END_SYNC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(process.env.PORT || 3000, () => {});
